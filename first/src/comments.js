5333b1823a8c7fd2cfb5857855a147b346a37dd8
MMunitz1 <144283149+MMunitz1@users.noreply.github.com>
2024-11-14 21:30:42 +0000

    hi

---
+++ first/src/comments.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import User from './comments'
+
+const User = (){
+    return(
+        <>
+        <section classname="contatianer">
+        <a> <img src={} </a>
+        <div className="content">
+            <a className="author">{Props.name}</a>
+        </div>
+        <div className="metadata">
+        <p>Today at <span className="data"></span></p>
+        </div>
+        </section>
+        </>
+    )
+}
